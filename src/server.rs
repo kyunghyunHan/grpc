bae2aae53cb8b7e2148dd7db1475829b09d81336
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-12-01 11:09:52 +0900

    add

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,9 @@
 use crate::pb::chat_server::Chat;
 
 pub struct ChatService;
+
+// #[tonic::async_trait]
+// impl Chat for ChatService {
+//     async fn login() {}
+//     async fn send_message() {}
+// }
