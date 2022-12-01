be2d7f039d812674c31425978c5663de171ac8ff
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-12-01 10:18:11 +0900

    add

--- build.rs
+++ build.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("dd");
 
     tonic_build::configure()
-        .type_attribute(".", "#[derive(Hash,Eq)]")
+        .type_attribute(
+            ".",
+            "#[derive(Hash,Eq,serde::Serialize,serde::Deserialize) ]",
+        )
         .out_dir("src/pb")
         .compile(&["./abi.proto"], &["./"])
         .unwrap();
+
+    println!("cargo:rerun-if-changed=./abi.proto");
+    println!("cargo:rerun-if-changed=./build.rs");
 }
