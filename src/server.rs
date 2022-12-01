a26558a1990de56efff880cdc93e4f42a03294a7
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-12-01 11:36:36 +0900

    add

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,27 @@
-use crate::pb::chat_server::Chat;
+use tonic::codegen::http::request;
+use tonic::{Request, Response, Status};
 
+use crate::pb::*;
+use crate::pb::{chat_server::Chat, LoginRequest};
 pub struct ChatService;
 
 // #[tonic::async_trait]
 // impl Chat for ChatService {
-//     async fn login() {}
-//     async fn send_message() {}
+//     async fn login(&self, request: Request<LoginRequest>) -> Result<Response<Token>, Status> {
+//         println!("login:{:?}", request.into_inner());
+//         todo!()
+//     }
+//     async fn send_message(
+//         &self,
+//         request: Request<NewChatMessage>,
+//     ) -> Result<Response<SendMessageResponse>, Status> {
+//         println!("send_message:{:?}", request.into_inner());
+//         todo!()
+//     }
+//     async fn get_messages(
+//         &self,
+//         request: Request<GetChatMessagesRequest>,
+//     ) -> ChatResult<GetMessagesRequest> {
+
+//     }
 // }
