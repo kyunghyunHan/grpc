4a9b32627aab8395fbecd173c632a76208956cd1
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-12-01 15:25:36 +0900

    add

--- src/server.rs
+++ src/server.rs
@@ -2,9 +2,14 @@ use crate::pb::*;
 use crate::pb::{chat_server::Chat, LoginRequest};
 use futures::prelude::*;
 use std::pin::Pin;
+use tokio::sync::{broadcast, mpsc};
+use tokio_stream::wrappers::UnboundedReceiverStream;
 use tonic::{Request, Response, Status};
 use tracing::info;
-pub struct ChatService;
+use tracing::log::warn;
+pub struct ChatService {
+    tx: broadcast::Sender<ChatMessage>,
+}
 pub type ChatResult<T> = Result<Response<T>, Status>;
 #[tonic::async_trait]
 impl Chat for ChatService {
@@ -23,7 +28,7 @@ impl Chat for ChatService {
         let info = request.into_inner();
         info!("send_message:{info:?}");
         let msg = info.into_chat_message(sender);
-
+        self.tx.send(msg).unwrap();
         Ok(Response::new(SendMessageResponse {}))
     }
     type GetMessagesStream = Pin<Box<dyn Stream<Item = Result<ChatMessage, tonic::Status>> + Send>>;
@@ -32,7 +37,21 @@ impl Chat for ChatService {
         &self,
         request: Request<GetMessagesRequest>,
     ) -> ChatResult<Self::GetMessagesStream> {
-        println!("get_messages:{:?}", request.into_inner());
-        todo!()
+        let info = request.into_inner();
+        let mut rx = self.tx.subscribe();
+        info!("subscribe to message{info:?}");
+        let (sender, receiver) = mpsc::unbounded_channel();
+
+        tokio::spawn(async move {
+            while let Ok(msg) = rx.recv().await {
+                if let Err(_) = sender.send(Ok(msg)) {
+                    warn!("Failed to send.Sender might be closed");
+                }
+            }
+        });
+
+        let stream = UnboundedReceiverStream::new(receiver);
+
+        Ok(Response::new(Box::pin(stream)))
     }
 }
